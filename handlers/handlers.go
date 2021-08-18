b105976f9ff970f3e37d92c84528ae96b0d991ed
jamesvrooney <jamesvrooney@hotmail.com>
2021-08-18 22:55:16 +0100

    Added custom hello handler.

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,10 +2,31 @@ package handlers
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
+// Hello handler for requests to /hello
+type Hello struct {
+	log *log.Logger
+}
+
+// NewHello creates a new Hello handler
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
 // SayHello say hello
-func SayHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello James...</h1>")
+func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.log.Println("")
+	d, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, "Oops", http.StatusBadRequest)
+
+		return
+	}
+
+	fmt.Fprintf(w, "<h1>Hello James %s</h1>", d)
 }
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"jamesvrooney/microservices/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.SayHello)
+	logger := log.New(os.Stdin, "product-api ", log.LstdFlags)
 
-	http.ListenAndServe(":9898", nil)
+	helloHandler := handlers.NewHello(logger)
+
+	sm := http.NewServeMux()
+
+	sm.Handle("/hello", helloHandler)
+
+	http.ListenAndServe(":9898", sm)
 }
